286fc355cc7eb19183e49ae26ab02d33624e710b
TGraceAn <annt.bi12-008@st.usth.edu.vn>
2024-05-21 15:18:39 +0200

    added some data analysis

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ Write a reversed translation code from txt files to midi files <br>
 Write a code to read the user midi files (which in different format) and convert them to txt files <br>
 Write a code to separate different precussion instruments <br>
 Do encoder stuff <br>
+Write a script to check max (+) transpose augmentation and min (-) transpose augmentation <br>
---
+++ module/data_analysis.py
@@ -0,0 +1,73 @@
+import regex as re
+import os
+
+def analyze_data(path):
+    max_note = find_highestnote(path)
+    min_note = find_lowestnote(path)
+    
+    return max_note, min_note
+                            
+
+def find_highestnote(path):
+    max_note = 0
+    for files in os.listdir(path):
+        if files.endswith('+6.txt'):
+            file_name = files[0:-6]
+            with open(f'{path}/{file_name}+6.txt', 'r') as f:
+                data = f.readlines()
+                track4sub = False
+                for line in data:
+                    track_start = re.search(r'\[TRACK_START\]', line)
+                    if track_start:
+                        track4sub = True
+                    precussion = re.search(r'\[INSTRUMENT\] \d+\.', line)
+                    if precussion:
+                        track4sub = False
+                    if track4sub == True:
+                        note_represent = re.search(r'(\d+ \d+ \d+ \d+)', line)
+                        if note_represent:
+                            note_represent = note_represent.group(1)
+                            note_represent = note_represent.split(' ')
+                            if int(note_represent[0]) == 0:
+                                if int(note_represent[1]) > max_note:
+                                    max_note = int(note_represent[1])
+    return max_note
+
+def find_lowestnote(path):
+    min_note = 127
+    for files in os.listdir(path):
+        if files.endswith('-5.txt'):
+            file_name = files[0:-6]
+            with open(f'{path}/{file_name}-5.txt', 'r') as f:
+                data = f.readlines()
+                track4sub = False
+                for line in data:
+                    track_start = re.search(r'\[TRACK_START\]', line)
+                    if track_start:
+                        track4sub = True
+                    precussion = re.search(r'\[INSTRUMENT\] \d+\.', line)
+                    if precussion:
+                        track4sub = False
+                    if track4sub == True:
+                        note_represent = re.search(r'(\d+ \d+ \d+ \d+)', line)
+                        if note_represent:
+                            note_represent = note_represent.group(1)
+                            note_represent = note_represent.split(' ')
+                            if int(note_represent[0]) == 0:
+                                if int(note_represent[1]) < min_note:
+                                    if int(note_represent[1]) == 0:
+                                        print(file_name)
+                                        print(note_represent)
+                                        pass
+                                    else:
+                                        min_note = int(note_represent[1])
+                                
+    return min_note
+
+def find_avgnote():
+    # Code to find the average note in the data
+    pass
+
+def find_medianote():
+    # Code to find the median note in the data
+    pass
